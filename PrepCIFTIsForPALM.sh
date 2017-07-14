545bc1b1166de40c41dddfb8c4b5b311f0c36378
Eric Feczko <feczko@rushmore.ohsu.edu>
2017-07-14 13:27:35 -0700

    updated documentation for PrepCIFTIsforPALM.sh

--- PrepCIFTIsForPALM.sh
+++ PrepCIFTIsForPALM.sh
@@ -1,4 +1,22 @@
 #! /bin/bash
+#PrepCIFTIsForPALM.sh is used to prepare CIFTI and surface GIFTI files for analysis using FSL's Permutation Analysis of Linear Models
+#A parameter file is required to use PrepCIFTIsForPALM. One should have the following files before running this script:
+#
+#parameter_file.params -- parameter file containing the options for running PALM, and directs I/O (Inputs/Outputs)
+#concfile.conc -- a single column text file with the paths for each subject's HCP data (up to where the MNINonLinear folder is located)
+#scalarfile.conc -- a single column text file  with the paths for each subject's dscalar data (should match the number and order of the concfile.conc, or errors will occur when running PALM)
+#design_matrix.txt -- the design matrix as a tab-delimited text file, either handmade or output from PalmReader.m
+#contrast_matrix.txt -- the contrast (weight) matrix  as a tab-delimited text file, either handmade or output from PalmReader.m
+#
+#The files below are optional for ANOVA and/or repeated measures analyses:
+#
+#ftest_matrix.txt -- the ftest matrix for anovas and repeated measures as a tab-delimited text file, either handmade or output from PalmReader.m
+#rm_matrix.txt -- the "groups" matrix for repeated measures, determines which cases are exchangable for constructing appropriate permutations. Also can be handmade or output from PalmReader.m
+#
+#In addition, make sure all subjects have the appropriate MNINonLinear/fsaverage_LR32k/*L.midthickness*surf.gii and corresponding RH surface. In a pinch, the midthickness can be replaced with other surfaces.
+#
+#
+#Usage: PrepCIFTIsForPALM.sh parameter_file.params
 source $1
 export PATH=$PATH:/usr/share/fsl/5.0/bin/ #sets the path to use Text2Vest change or comment out depending on the version
 if [ -f design_matrix.txt ]; then Text2Vest design_matrix.txt design.mat; fi
@@ -110,6 +128,19 @@ echo "-o LEFT_CORTEX" >> ${output_directory}/PALManalysis/L_func.cfg
 echo "-o RIGHT_CORTEX" >> ${output_directory}/PALManalysis/R_func.cfg
 echo "-o VOLUME" >> ${output_directory}/PALManalysis/VOL_func.cfg
 #the instructions below will run PALM on each of the three datasets
+touch ${output_directory}/palm_instructions.txt
+echo "PALM files are prepared. To run palm follow these steps:"
+echo " 1) cd ${output_directory}/PALManalysis"
+echo " 2) palm L_func.cfg"
+echo " 3) palm R_func.cfg"
+echo " 4) palm VOL_func.cfg"
+echo "For efficiency, it is recommended to run palm for the three cfg files (L,R,VOL) in separate processes."
+echo "PALM files are prepared. To run palm follow these steps:" >> ${output_directory}/palm_instructions.txt
+echo " 1) cd ${output_directory}/PALManalysis" >> ${output_directory}/palm_instructions.txt
+echo " 2) palm L_func.cfg" >> ${output_directory}/palm_instructions.txt
+echo " 3) palm R_func.cfg" >> ${output_directory}/palm_instructions.txt
+echo " 4) palm VOL_func.cfg" >> ${output_directory}/palm_instructions.txt
+echo "For efficiency, it is recommended to run palm for the three cfg files (L,R,VOL) in separate processes." >> ${output_directory}/palm_instructions.txt
 #pushd ${output_directory}/PALManalysis
 #palm L_func.cfg
 #palm R_func.cfg
