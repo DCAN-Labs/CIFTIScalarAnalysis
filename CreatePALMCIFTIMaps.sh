23d56497d79f1ee2910905592687492841223477
Eric Feczko <feczko@rushmore.ohsu.edu>
2017-07-17 11:06:53 -0700

    Modified CreatePALMCIFTIMaps to ignore volume files and handle clean up if specifcied

--- CreatePALMCIFTIMaps.sh
+++ CreatePALMCIFTIMaps.sh
@@ -2,6 +2,7 @@
 #CreatePALMCIFTIMaps.sh is used to generate CIFTI maps from PALM statistical outputs. 
 #Simply provide the paramfile used in PrepCIFTIsForPALM.sh and the files will be generated.
 #If PALM outputs were not generated using PrepCIFTIsForPALM.sh, make sure to rename the cortical and volumetric outputs accordingly (see below)
+#If "clean_up_data" is set to true in the parameter file. The merged data folder will be deleted, to save space though the subject scalar file will be maintained to reproduce the maps.
 #NOTE: after generating the CIFTI maps, and double-checking to make sure they are correct, one can archive the ouput from the CIFTI analysis.
 #
 #
@@ -13,9 +14,9 @@ pushd ${output_directory}/PALManalysis/
 mkdir ../CIFTI_outputs
 for image in `ls VOLUME_*.nii`; do 
 	statfile=`expr "$image" | sed s/VOLUME_// | sed s/.nii//`
- 	if [ `expr "$blah" : \vox` == 0 ]; then
+ 	if [ `expr "$statfile" : \vox` == 0 ]; then
 #edit the prefixes (or rename the files) in the command below for VOLUME,LEFT_CORTEX,and RIGHT_CORTEX, if generated differently. Or just rename your outputs :)
 		wb_command -cifti-create-dense-from-template ../merged_data/all_data.dscalar.nii ../CIFTI_outputs/results_${statfile}.dscalar.nii -volume-all VOLUME_${statfile}.nii -metric CORTEX_LEFT LEFT_CORTEX_${statfile}.gii -metric CORTEX_RIGHT RIGHT_CORTEX_${statfile}.gii 
 	fi
 done
-
+if $clean_up_data; then mv ${output_directory}/merged_data/all_data.dscalar.nii ${output_directory}/ ; rm -rf ${output_directory}/merged_data; fi
