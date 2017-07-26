d19a47a938d9f345b62eaa71391d41560bee1cb0
Eric Feczko <feczko@airclogin-a1.ohsu.edu>
2017-07-26 11:28:24 -0700

    modified RunSlurmOnConcDir.sh to add option for stdout from jobs

--- RunSlurmOnConcDir.sh
+++ RunSlurmOnConcDir.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 conc_path=$1
+job_path=$2
+mkdir ${job_path}
 conc_files=`ls ${conc_path}/*.params`
 iterjob=1
 for conc in ${conc_files}
-	do ssub -t 120:: -J conc_${iterjob} PrepCIFTIsForPALM.sh ${conc}
+	do ssub -t 120:: -J ${job_path}/conc_${iterjob} PrepCIFTIsForPALM.sh ${conc}
 	iterjob=`expr ${iterjob} + 1`
 	sleep 5
 done
