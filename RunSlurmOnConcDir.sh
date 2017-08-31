f4adb08061411afb5741e646f84b60345be10af7
feczko <feczko@metal-1-0.local>
2017-08-31 13:15:46 -0700

    fixed bug with Exacloud for submitting srun jobs

--- RunSlurmOnConcDir.sh
+++ RunSlurmOnConcDir.sh
@@ -7,7 +7,7 @@ mkdir ${job_path}
 conc_files=`ls ${conc_path}/*.params`
 iterjob=1
 for conc in ${conc_files}
-	do srun --quiet -o ${job_path}/conc_${iterjob}.out -e ${job_path}/conc_${iterjob}.err --partition ${partition} PrepCIFTIsForPALM.sh ${conc} & disown
+	do srun --quiet -o ${job_path}/conc_${iterjob}.out -e ${job_path}/conc_${iterjob}.err --partition ${partition} --time 1-12:0 PrepCIFTIsForPALM.sh ${conc} & disown
 	iterjob=`expr ${iterjob} + 1`
 	sleep 5
 done
